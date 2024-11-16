b9d524af7624ed116758b6982fa440609fc911c7
Rhys How <rhow1@sheffield.ac.uk>
2024-11-16 13:57:50 +0000

    added positions to the character

--- classes/character.py
+++ classes/character.py
@@ -1,9 +1,11 @@
 class Character:
-    def __init__(self, name, sprite, inventory):
+    def __init__(self, name, sprite, inventory, posx, posy):
         self.name = name
         self.sprite = sprite
         self.health = 100
         self.inventory = inventory
+        self.posx = posx
+        self.posy = posy
 
     def getName(self):
         return self.name
@@ -11,3 +13,7 @@ class Character:
         return self.health
     def getInventory(self):
         return self.inventory
+    def getPosx(self):
+        return self.posx
+    def getPosy(self):
+        return self.posy
