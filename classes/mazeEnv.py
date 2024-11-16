6cc3b90f52cb3a6bd81e5dfe4cd7c64d8a2d43a4
Jamie-084 <jgrey1@sheffield.ac.uk>
2024-11-16 22:44:09 +0000

    Merged Rhys' code

--- classes/mazeEnv.py
+++ classes/mazeEnv.py
@@ -6,6 +6,8 @@ class MazeEnv:
         self.size = size
         self.obstacles = obstacles
         self.visited = None  # To track visited squares
+        self.agent_pos = [0,0]
+        self.goal_pos = [self.size -1, self.size -1]
         self.reset()
     
     def reset(self):
@@ -13,8 +15,16 @@ class MazeEnv:
         self.goal_pos = [self.size - 1, self.size - 1]  # Goal at the bottom-right corner
         self.visited = np.zeros((self.size, self.size))  # Reset visited squares
         self.visited[0, 0] = 1  # Mark starting position as visited
+        self.map_memory = np.zeros((self.size, self.size))
+        self.update_map_memory(self.agent_pos,1)
+        for obstacle in self.obstacles:
+            self.update_map_memory(obstacle, -1)
+        self.update_map_memory(self.goal_pos, 2)
         self.path = [self.agent_pos.copy()]
         return self.get_state()
+    
+    def update_map_memory(self, position, value):
+        self.map_memory[position[0], position[1]] = value
     
     def get_state(self):
         return [self.agent_pos[0], self.agent_pos[1], self.goal_pos[0], self.goal_pos[1]]
@@ -28,12 +38,16 @@ class MazeEnv:
         if (0 <= new_pos[0] < self.size and 0 <= new_pos[1] < self.size) and new_pos not in self.obstacles:
             self.agent_pos = new_pos
 
+
         # Track the path
         self.path.append(self.agent_pos.copy())
+
         
         # Reward for reaching the goal
         if self.agent_pos == self.goal_pos:
-            return self.get_state(), 1.0, True  # Reward for reaching the goal
+            return self.get_state(), 10.0, True  # Reward for reaching the goal
+        else:
+            return self.get_state(), -0.01, False
         
          # Exploration bonus for unvisited squares
         exploration_bonus = 0
@@ -51,6 +65,9 @@ class MazeEnv:
         grid = np.zeros((self.size, self.size))
         grid[self.goal_pos[0], self.goal_pos[1]] = 2  # Goal
         grid[self.agent_pos[0], self.agent_pos[1]] = 1  # Agent's position
+
+        for obstacle in self.obstacles:
+            grid[obstacle[0], obstacle[1]] = -1
         
         plt.figure(figsize=(6, 6))
         plt.imshow(grid, cmap='coolwarm', origin='upper')
@@ -65,4 +82,5 @@ class MazeEnv:
         plt.xticks(range(self.size))
         plt.yticks(range(self.size))
         plt.grid(True)
+        plt.title("Maze Environment")
         plt.show()
